c5169a6ffc51b06b94cc5109e17da75c3c37eba2
mhoja <mhoja@consdata.pl>
2019-08-16 15:17:51 +0200

    add ViewEncapsulation.None

--- src/app/green-module/green.component.ts
+++ src/app/green-module/green.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 
 @Component({
     selector: 'app-green',
@@ -9,7 +9,8 @@ import {Component} from '@angular/core';
         p {
             color: green;
         }
-    `]
+    `],
+    encapsulation: ViewEncapsulation.None
 })
 export class GreenComponent {
 }
--- src/app/red-module/red.component.ts
+++ src/app/red-module/red.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 
 @Component({
     selector: 'app-red',
@@ -9,7 +9,8 @@ import {Component} from '@angular/core';
         p {
             color: red;
         }
-    `]
+    `],
+    encapsulation: ViewEncapsulation.None
 })
 export class RedComponent {
 }
