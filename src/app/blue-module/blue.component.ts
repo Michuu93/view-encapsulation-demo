064a2776db4994cc740b665176b8b626d4cf37f7
mhoja <mhoja@consdata.pl>
2019-08-21 09:38:24 +0200

    move component green from root to blue

--- src/app/blue-module/blue.component.ts
+++ src/app/blue-module/blue.component.ts
@@ -3,6 +3,7 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'app-blue',
     template: `
+        <app-green></app-green>
         <p>Blue paragraph!</p>
     `,
     styles: [`
